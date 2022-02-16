1ffc7c2ad08e6e22e63f92b1e4ac30e2352510eb
sveta <donskov2000@internet.ru>
2022-02-16 18:55:49 +0300

    3 var

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String massag;
+
+    public String getMassag() {
+        return massag;
+    }
+
+    public void setMassag(String massag) {
+        this.massag = massag;
+    }
+}
